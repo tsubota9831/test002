5cdcc0c42ad24abc06d8a89dfe3b2df098fec6e5
50616029 <50616029@50616029-pc>
2017-10-16 17:14:45 +0900

    First Commit

---
+++ src/test002/test0021.java
@@ -0,0 +1,16 @@
+package test002;
+
+import java.util.Scanner;
+
+public class test0021 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan = new Scanner(System.in);
+System.out.println("Input Number > ");
+int n = Integer.parseInt(scan.next());
+
+System.out.println("Number = "+n);
+	}
+
+}
